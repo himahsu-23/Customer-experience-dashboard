066dc41147589cc19b76769432d327d79c8db2bb
Himanshu Tripathi <tripathihimanshu252@gmail.com>
2025-08-27 18:47:19 +0530

    main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from utils import extract_and_load
 import os
+import sys
 
-# Sirf File 1 test ke liye
-df1, _ = extract_and_load(
-    r"C:\Users\himan\Downloads\archive (1).zip",
-    os.path.join("extracted_files", "file1")
-)
+# Check if running on Streamlit Cloud
+if "STREAMLIT_SERVER" in os.environ:
+    zip_path = "archive.zip"  # file in repo
+else:
+    zip_path = r"C:\Users\himan\Downloads\archive (1).zip"  # local path
+
+extract_path = os.path.join("extracted_files", "file1")
+df1, _ = extract_and_load(zip_path, extract_path)
 
 print("\n🔹 Columns in File 1 (Sales Data):")
 print(df1.columns.tolist())
